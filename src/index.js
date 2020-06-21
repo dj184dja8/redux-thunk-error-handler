657f1ea35b8e655d582b8ea8235330a2ce33f8eb
dj184dja8 <dj184dja8@gmail.com>
2020-06-21 16:13:14 +0800

    add error handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 function createThunkMiddleware(extraArgument) {
-  return ({ dispatch, getState }) => (next) => (action) => {
+  return (errorHandler) => ({ dispatch, getState }) => (next) => (action) => {
     if (typeof action === 'function') {
-      return action(dispatch, getState, extraArgument);
+      let result = null;
+      try {
+        result = action(dispatch, getState, extraArgument);
+      } catch (err) {
+        if (errorHandler) {
+          dispatch(errorHandler(err));
+        } else {
+          throw err;
+        }
+      }
+
+      if (result instanceof Promise) {
+        result = result
+          .then((res) => res)
+          .catch((err) => {
+            if (errorHandler) {
+              dispatch(errorHandler(err));
+            } else {
+              throw err;
+            }
+          });
+      }
+      return result;
     }
 
     return next(action);
